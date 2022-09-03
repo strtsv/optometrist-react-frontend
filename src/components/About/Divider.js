b85d1dfe9606828a28ea58bec56557edbf81224d
jsusilo <enr@bk.ru>
2022-09-04 01:23:28 +0500

    about comp

---
+++ src/components/About/Divider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Divider extends React.Component {
+  render() {
+    return <div className="divider-line wow fadeInLeft" data-wow-delay=".3s" />;
+  }
+}
+
+export default Divider;
