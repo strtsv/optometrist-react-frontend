5c8621a4c4e98c45f10dc63d8dce52cdb8561469
jsusilo <enr@bk.ru>
2022-09-05 23:54:58 +0500

    About WhatWeOffer

---
+++ src/components/About/WhatWeOffer.js
@@ -0,0 +1,108 @@
+import React from "react";
+
+class WhatWeOffer extends React.Component {
+  render() {
+    return (
+      <section className="section section-xl bg-default">
+        <div className="container">
+          <h2
+            className="text-uppercase text-center wow fadeInUp"
+            data-wow-delay=".2s"
+          >
+            What We Offer
+          </h2>
+          <p
+            className="text-center offset-top-50 fadeInUp"
+            data-wow-delay=".3s"
+          >
+            What We Offer Lorem ipsum dolor sit amet consectetuer adipiscing
+            elit. Praesent vestibulum molestie lacus. Aeneanummy hendrerit
+            mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis
+            natoque penatibus et magnis diturient ontes nascetur ridiculula.
+            Praesent vestibulum molestie lacus. Aenean nonummy hendrerit.
+          </p>
+          <div className="row row-40 offset-top-0 offset-top-60">
+            <div
+              className="col-sm-6 col-lg-3 wow fadeInLeft"
+              data-wow-delay=".1s"
+            >
+              <div className="box-circle">
+                <div className="box-circle-inner">01</div>
+                <h3 className="lh-2">
+                  <a className="link-secondary" href="#">
+                    Full Spectrum of Eye Care
+                  </a>
+                </h3>
+                <p className="lh-1 opacity-80">
+                  Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                  Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                  mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis
+                  natoque penatibus magnis.
+                </p>
+              </div>
+            </div>
+            <div
+              className="col-sm-6 col-lg-3 wow fadeInLeft"
+              data-wow-delay=".2s"
+            >
+              <div className="box-circle">
+                <div className="box-circle-inner">02</div>
+                <h3 className="lh-2">
+                  <a className="link-secondary" href="#">
+                    Praesent vestibulum mol
+                  </a>
+                </h3>
+                <p className="lh-1 opacity-80">
+                  Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                  Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                  mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis
+                  natoque penatibus magnis.
+                </p>
+              </div>
+            </div>
+            <div
+              className="col-sm-6 col-lg-3 wow fadeInLeft"
+              data-wow-delay=".3s"
+            >
+              <div className="box-circle">
+                <div className="box-circle-inner">03</div>
+                <h3 className="lh-2">
+                  <a className="link-secondary" href="#">
+                    Sed ut perspiciatis unde
+                  </a>
+                </h3>
+                <p className="lh-1 opacity-80">
+                  Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                  Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                  mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis
+                  natoque penatibus magnis.
+                </p>
+              </div>
+            </div>
+            <div
+              className="col-sm-6 col-lg-3 wow fadeInLeft"
+              data-wow-delay=".4s"
+            >
+              <div className="box-circle">
+                <div className="box-circle-inner">04</div>
+                <h3 className="lh-2">
+                  <a className="link-secondary" href="#">
+                    Ut enim minima veniam
+                  </a>
+                </h3>
+                <p className="lh-1 opacity-80">
+                  Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                  Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                  mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis
+                  natoque penatibus magnis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default WhatWeOffer;
