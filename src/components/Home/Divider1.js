be30193a5bf03b1a897e943bbf84fb6bc8977be9
jsusilo <enr@bk.ru>
2022-09-05 23:56:39 +0500

    home dividers

---
+++ src/components/Home/Divider1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Divider1 extends React.Component {
+  render() {
+    return <div className="divider-line wow fadeInLeft" data-wow-delay=".3s" />;
+  }
+}
+
+export default Divider1;
---
+++ src/components/Home/Divider2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Divider2 extends React.Component {
+  render() {
+    return <div className="divider-line-2 wow fadeInUp" data-wow-delay=".3s" />;
+  }
+}
+
+export default Divider2;
