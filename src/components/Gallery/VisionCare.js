7269c1d2dc734f881728860adee2c9b1d32de3fc
tenoclock <enr@bk.ru>
2022-09-12 00:46:31 +0500

    Gallery VisionCare

---
+++ src/components/Gallery/VisionCare.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class VisionCare extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg bg-default">
+        <div className="container">
+          <h2
+            className="text-uppercase text-center wow fadeInUp"
+            data-wow-delay=".3s"
+          >
+            Optometrist Care Services For The
+            <br className="d-none d-xl-block" /> Whole Family
+          </h2>
+          <h3
+            className="lh-2 text-primary text-center wow fadeInLeft"
+            data-wow-delay=".2s"
+          >
+            Lorem ipsum dolor sit amet consectetuer adipiscing elit. Praesent
+            vestibulum molestie lacus.
+          </h3>
+          <p
+            className="lh-1 text-center wow fadeInRight offset-top-10"
+            data-wow-delay=".2s"
+          >
+            Lorem ipsum dolor sit amet consectetuer adipiscing elit. Praesent
+            vestibulum molestie lacus. Aeneanummy hendrerit mauris. Phasellus
+            porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et
+            magnis diturient ontes nascetur ridiculula. Praesent vestibulum
+            molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.
+            Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis
+            parturient montes, nascetur ridiculus mus. Nulla dui.
+          </p>
+          <div className="text-center offset-top-40">
+            <a
+              className="button button-lg button-primary wow fadeInUp"
+              href="#"
+              data-wow-delay=".3s"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default VisionCare;
