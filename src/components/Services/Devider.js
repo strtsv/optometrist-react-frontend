1b50f5d2cbb3042fb96dee632b23ca9c4cf4b89e
tenoclock <enr@bk.ru>
2022-09-14 00:04:55 +0500

    services common

---
+++ src/components/Services/Devider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Devider extends React.Component {
+  render() {
+    return <div className="divider-line wow fadeInLeft" data-wow-delay=".3s" />;
+  }
+}
+
+export default Devider;
