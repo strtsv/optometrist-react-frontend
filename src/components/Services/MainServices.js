267ff276d58f90cb4a9e3a164000bc6d98c9df9a
tenoclock <enr@bk.ru>
2022-09-14 15:46:31 +0500

    Main Service

---
+++ src/components/Services/MainServices.js
@@ -0,0 +1,165 @@
+import React from "react";
+
+class MainServices extends React.Component {
+  render() {
+    return (
+      <section className="section section-first section-xl bg-default">
+        <div className="container">
+          <h2
+            className="text-uppercase text-center wow fadeInUp"
+            data-wow-delay=".2s"
+          >
+            Main Services
+          </h2>
+          <p className="lh-1 text-center">
+            Lorem ipsum dolor sit amet consectetuer adipiscing elit. Praesent
+            vestibulum molestie lacus. Aeneanummy hendrerit mauris. Phasellus
+            porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et
+            magnis diturient ontes nascetur ridiculula. Praesent vestibulum
+            molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.
+            Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis
+            parturient montes, nascetur ridiculus mus. Nulla dui.
+          </p>
+          <div className="row row-40 row-xl-50 offset-top-0 offset-top-60">
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".2s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-link73" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Lorem ipsum dolor
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".3s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-share16" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Phasellus portausc
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".4s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-screen85" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Aenean nec eros
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".5s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-magnifying-glass17" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Lorem ipsum dolor
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".6s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-house181" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Phasellus portausc
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-md-6 col-xl-4 wow fadeInRight"
+              data-wow-delay=".7s"
+            >
+              <div className="box-services">
+                <div className="box-services-media">
+                  <span className="icon text-primary icon-lg fl-sympletts-glasses" />
+                </div>
+                <div className="box-services-text">
+                  <h3>
+                    <a className="link-secondary" href="#">
+                      Aenean nec eros
+                    </a>
+                  </h3>
+                  <p className="lh-1 opacity-80">
+                    Lorem ipsum dolor sit amet consec tetuer adipiscing elit.
+                    Praesent ves tibulum molestie lacus.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="text-center wow fadeInRight offset-top-80"
+            data-wow-delay=".7s"
+          >
+            <a className="button button-lg button-primary" href="#">
+              All Services
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MainServices;
