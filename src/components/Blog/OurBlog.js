18441ab479a13da5e5c139460c81939aa7354dfb
tenoclock <enr@bk.ru>
2022-09-25 00:01:54 +0500

    blog content

---
+++ src/components/Blog/OurBlog.js
@@ -0,0 +1,215 @@
+import React from "react";
+
+import i1 from "../../assets/images/page4_pic1.jpg";
+import i2 from "../../assets/images/page4_pic2.jpg";
+import i3 from "../../assets/images/page4_pic3.jpg";
+import i4 from "../../assets/images/page4_pic4.jpg";
+
+class OurBlog extends React.Component {
+  render() {
+    return (
+      <section className="section section-xl bg-gray-100">
+        <div className="container">
+          <h2
+            className="text-uppercase text-center wow fadeInUp"
+            data-wow-delay=".2s"
+          >
+            Our Blog
+          </h2>
+          <div className="row row-60-xl-40 offset-top-0 offset-top-60">
+            <div className="col-12 wow fadeInleft" data-wow-delay=".2s">
+              <div className="box-blog">
+                <div className="media-inner">
+                  <img src={i1} alt />
+                </div>
+                <div className="text-inner">
+                  <h3 className="lh-0">
+                    <a className="link-secondary" href="#">
+                      Lorem ipsum dolor sit amet consectetuer
+                    </a>
+                  </h3>
+                  <div className="d-flex justify-content-between flex-column flex-sm-row offset-top-15">
+                    <span>
+                      Added 2 days ago by 
+                      <a className="link-secondary" href="#">
+                        Admin
+                      </a>
+                    </span>
+                    <span>
+                      <span className="icon mdi mdi-comment-outline text-primary">
+                           
+                      </span>
+                      <a href="#">3 comments</a>
+                    </span>
+                  </div>
+                  <p className="lh-1 offset-top-15">
+                    Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                    Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus magnis diturient ontes nascetur
+                    ridiculula. Ut enim ad minima veniam quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisium.
+                    Praesent vestibulum molestie lacus. Aenean nonummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada
+                    odio.
+                  </p>
+                  <a
+                    className="button button-lg button-primary offset-top-40"
+                    href="#"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 wow fadeInRight" data-wow-delay=".2s">
+              <div className="box-blog">
+                <div className="media-inner">
+                  <img src={i2} alt />
+                </div>
+                <div className="text-inner">
+                  <h3 className="lh-0">
+                    <a className="link-secondary" href="#">
+                      Praesent vestibulum molestie lacusem
+                    </a>
+                  </h3>
+                  <div className="d-flex justify-content-between flex-column flex-sm-row offset-top-15">
+                    <span>
+                      Added 2 days ago by 
+                      <a className="link-secondary" href="#">
+                        Admin
+                      </a>
+                    </span>
+                    <span>
+                      <span className="icon mdi mdi-comment-outline text-primary">
+                           
+                      </span>
+                      <a href="#">3 comments</a>
+                    </span>
+                  </div>
+                  <p className="lh-1 offset-top-15">
+                    Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                    Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus magnis diturient ontes nascetur
+                    ridiculula. Ut enim ad minima veniam quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisium.
+                    Praesent vestibulum molestie lacus. Aenean nonummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada
+                    odio.
+                  </p>
+                  <a
+                    className="button button-lg button-primary offset-top-40"
+                    href="#"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 wow fadeInLeft" data-wow-delay=".2s">
+              <div className="box-blog">
+                <div className="media-inner">
+                  <img src={i3} alt />
+                </div>
+                <div className="text-inner">
+                  <h3 className="lh-0">
+                    <a className="link-secondary" href="#">
+                      Sed ut perspiciatis unde omnis iste natus
+                    </a>
+                  </h3>
+                  <div className="d-flex justify-content-between flex-column flex-sm-row offset-top-15">
+                    <span>
+                      Added 2 days ago by 
+                      <a className="link-secondary" href="#">
+                        Admin
+                      </a>
+                    </span>
+                    <span>
+                      <span className="icon mdi mdi-comment-outline text-primary">
+                           
+                      </span>
+                      <a href="#">3 comments</a>
+                    </span>
+                  </div>
+                  <p className="lh-1 offset-top-15">
+                    Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                    Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus magnis diturient ontes nascetur
+                    ridiculula. Ut enim ad minima veniam quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisium.
+                    Praesent vestibulum molestie lacus. Aenean nonummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada
+                    odio.
+                  </p>
+                  <a
+                    className="button button-lg button-primary offset-top-40"
+                    href="#"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 wow fadeInRight" data-wow-delay=".2s">
+              <div className="box-blog">
+                <div className="media-inner">
+                  <img src={i4} alt />
+                </div>
+                <div className="text-inner">
+                  <h3 className="lh-0">
+                    <a className="link-secondary" href="#">
+                      Ut enim ad minima veniam quis nostrum
+                    </a>
+                  </h3>
+                  <div className="d-flex justify-content-between flex-column flex-sm-row offset-top-15">
+                    <span>
+                      Added 2 days ago by 
+                      <a className="link-secondary" href="#">
+                        Admin
+                      </a>
+                    </span>
+                    <span>
+                      <span className="icon mdi mdi-comment-outline text-primary">
+                           
+                      </span>
+                      <a href="#">3 comments</a>
+                    </span>
+                  </div>
+                  <p className="lh-1 offset-top-15">
+                    Lorem ipsum dolor sit amet consectetuer adipiscing elit.
+                    Praesent vestibulum molestie lacus. Aenean ummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus magnis diturient ontes nascetur
+                    ridiculula. Ut enim ad minima veniam quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisium.
+                    Praesent vestibulum molestie lacus. Aenean nonummy hendrerit
+                    mauris. Phasellus porta. Fusce suscipit varius mi. Cum
+                    sociis natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada
+                    odio.
+                  </p>
+                  <a
+                    className="button button-lg button-primary offset-top-40"
+                    href="#"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurBlog;
